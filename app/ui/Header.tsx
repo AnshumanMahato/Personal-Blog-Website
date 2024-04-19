97820e2eaad533da8c3ee7a5546a4737b4022f2d
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2024-04-19 11:40:49 +0530

    feat: header responsive

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -2,9 +2,16 @@ import { IoHomeOutline, IoMenu } from "react-icons/io5";
 
 function Header() {
   return (
-    <header className="flex justify-between items-center  p-[2rem] text-[2.5rem] leading-[140%] text-white font-semibold w-full max-w-[800px]">
+    <header className="flex justify-between items-center  p-[2rem] xs:p-[3rem] text-[2.5rem] xs:text-[3rem] leading-[140%] text-white w-full max-w-[800px]">
       <IoHomeOutline />
-      <IoMenu />
+      <IoMenu className="md:hidden" />
+      <nav className="hidden md:block">
+        <ul className="flex text-[1.6rem] gap-[3rem]">
+          <li>About</li>
+          <li>Projects</li>
+          <li>Blog</li>
+        </ul>
+      </nav>
     </header>
   );
 }
