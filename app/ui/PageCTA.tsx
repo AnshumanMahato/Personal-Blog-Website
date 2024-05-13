001052f66d92d82ac84cb3d7c0d8278212674fbc
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2024-05-13 20:08:17 +0530

    fix: fixed sticky links scrolling beyond the set area

--- app/ui/PageCTA.tsx
+++ app/ui/PageCTA.tsx
@@ -6,12 +6,14 @@ function PageCTA({
   links: { href: string; icon: React.ReactNode; handle: string }[];
 }) {
   return (
-    <section className="md:sticky top-32 md:row-start-2 md:col-start-2 flex flex-col items-start gap-[1.5rem] md:w-min h-min md:[justify-self:end] md:mt-[2.5rem]">
-      {links.map(({ href, icon, handle }) => (
-        <SocialLink key={handle} href={href} icon={icon}>
-          {handle}
-        </SocialLink>
-      ))}
+    <section className="md:row-start-2 md:col-start-2 flex flex-col md:items-end md:mt-[2.5rem]">
+      <div className="md:sticky top-32 flex flex-col gap-[1.5rem]">
+        {links.map(({ href, icon, handle }) => (
+          <SocialLink key={handle} href={href} icon={icon}>
+            {handle}
+          </SocialLink>
+        ))}
+      </div>
     </section>
   );
 }
