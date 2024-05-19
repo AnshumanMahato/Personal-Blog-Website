9a163d841aaefeff93c72d44a923cc14b9902a57
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2024-05-19 23:08:29 +0530

    feat: fetch post by slug action

---
+++ app/actions/getSinglePost.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import {
+  PostFullFragment,
+  PublicationFragment,
+  SinglePostByPublicationDocument,
+  SinglePostByPublicationQuery,
+} from "@/app/schema/graphql";
+
+type PostInfo = {
+  post: PostFullFragment | null | undefined;
+  publication: PublicationFragment;
+};
+
+async function getSinglePost(slug: string): Promise<PostInfo | null> {
+  const GQL_ENDPOINT: string = process.env.NEXT_PUBLIC_HASHNODE_GQL_ENDPOINT!;
+  const response = await fetch(GQL_ENDPOINT, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: SinglePostByPublicationDocument,
+      variables: {
+        host: process.env.NEXT_PUBLIC_HASHNODE_PUBLICATION_HOST,
+        slug,
+      },
+    }),
+    cache: "force-cache",
+  });
+
+  const {
+    data: { publication },
+  }: { data: SinglePostByPublicationQuery } = await response.json();
+
+  if (!publication) return null;
+
+  return {
+    post: publication.post,
+    publication,
+  };
+}
+
+export default getSinglePost;
