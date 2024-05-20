b196082e51e9e8d242295a542bb9ef870c87c093
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2024-05-20 19:59:38 +0530

    feat: generate metadata for blogposts

--- app/(slugs)/blogs/[slug]/page.tsx
+++ app/(slugs)/blogs/[slug]/page.tsx
@@ -1,5 +1,37 @@
-function About() {
-  return <div>About</div>;
+import getSinglePost from "@/app/actions/getSinglePost";
+import { ResolvingMetadata } from "next";
+
+type Props = Readonly<{
+  params: {
+    slug: string;
+  };
+}>;
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+) {
+  const data = await getSinglePost(params.slug);
+  if (!data) return;
+  const { post, publication } = data;
+
+  return {
+    title: post?.seo?.title ?? post?.title,
+    description: post?.seo?.description || post?.subtitle || post?.brief,
+    alternates: {
+      canonical: post?.canonicalUrl ?? post?.url,
+    },
+    twitter: {
+      card: "summary_large_image",
+      site: "@AnshumanMahato_",
+      title: post?.seo?.title ?? post?.title,
+      description: post?.seo?.description || post?.subtitle || post?.brief,
+    },
+  };
 }
 
-export default About;
+function BlogPost({ params }: Props) {
+  return <div>{params.slug}</div>;
+}
+
+export default BlogPost;
