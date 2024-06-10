f88cbdc0763590228f66e3e67b89e50ad01efb0a
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2024-06-10 12:45:30 +0530

    feat: sitemap integrated
    Signed-off-by: AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>

---
+++ app/actions/getPostsSitemapData.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { SitemapDocument, SitemapQuery } from "@/app/schema/graphql";
+import PostsSitemapInfo from "../@types/PostsSitemapInfo";
+
+async function getPostsSitemapData(
+  after?: string
+): Promise<PostsSitemapInfo | null> {
+  const GQL_ENDPOINT: string = process.env.NEXT_PUBLIC_HASHNODE_GQL_ENDPOINT!;
+  const response = await fetch(GQL_ENDPOINT, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: SitemapDocument,
+      variables: {
+        host: process.env.NEXT_PUBLIC_HASHNODE_PUBLICATION_HOST,
+        first: 20,
+        after,
+      },
+    }),
+    cache: "force-cache",
+  });
+
+  const {
+    data: { publication },
+  }: { data: SitemapQuery } = await response.json();
+
+  if (!publication) return null;
+  const posts = (publication.posts.edges ?? []).map((edge) => edge.node);
+
+  return {
+    posts,
+    pageInfo: publication.posts.pageInfo,
+  };
+}
+
+export default getPostsSitemapData;
