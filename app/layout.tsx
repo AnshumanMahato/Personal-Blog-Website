dce8df5ad92d404adcf8091b9969c512cfa6e2db
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2024-06-28 19:05:40 +0530

    fix: footer moved inside root element
    Signed-off-by: AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,9 +30,9 @@ export default function RootLayout({ children }: Props) {
         >
           <Header />
           {children}
+          <Footer />
           <DarkModeSwitch />
         </div>
-        <Footer />
       </body>
     </html>
   );
