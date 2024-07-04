eb5e9b28307fb4ba12dfece4bf142e1f4d7799b4
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2024-07-04 16:06:28 +0530

    feat: general error page init

---
+++ app/error.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <p>{error.message}</p>
+    </div>
+  );
+}
