db545b55c8bc25aedef63955435dd05b177f41bd
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2026-02-15 23:26:11 +0530

    feat: artbook gallery base

---
+++ app/(overview)/artbook/page.tsx
@@ -0,0 +1,119 @@
+import { FiLinkedin } from "react-icons/fi";
+import { FaXTwitter } from "react-icons/fa6";
+import { MdOutlineEmail } from "react-icons/md";
+import PageHeading from "@/app/components/PageHeading";
+import PageBanner from "@/app/components/PageBanner";
+import Section from "@/app/components/Section";
+import PageCTA from "@/app/components/PageCTA";
+import { Metadata } from "next";
+import { addAboutJsonLd } from "@/app/utils/seo/addAboutJsonLd";
+import profile from "@/app/lib/profile.json";
+import Link from "next/link";
+import Image from "next/image";
+import GoogleAnalytics from "@/app/components/GoogleAnalytics";
+
+export const metadata: Metadata = {
+  title: `Artbook | ${profile.name}`,
+  description: profile.headline,
+  twitter: {
+    card: "summary_large_image",
+    site: profile.handles.socials.twitter,
+    title: `Artbook | ${profile.name}`,
+    description: profile.headline,
+  },
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    title: `Artbook | ${profile.name}`,
+    description: profile.headline,
+  },
+};
+
+const { twitter, linkedin } = profile.handles.socials;
+
+function About() {
+  const socials = [
+    {
+      href: twitter,
+      icon: <FaXTwitter />,
+      handle: `@${twitter.split("/").pop()}`,
+    },
+    {
+      href: linkedin,
+      icon: <FiLinkedin />,
+      handle: `/${linkedin.split("/").pop()}`,
+    },
+    {
+      href: `mailto:${profile.email}`,
+      icon: <MdOutlineEmail />,
+      handle: profile.email,
+    },
+  ];
+
+  return (
+    <>
+      <GoogleAnalytics />
+      <PageBanner
+        title="Artbook"
+        coverImage="/assets/images/aboutcover.png"
+        coverImageAlt="artbook"
+      />
+      <Section className="col-span-full">
+        <PageHeading>I&apos;m Anshuman.</PageHeading>
+        <div className="columns-xs gap-[1rem]">
+          <Image
+            src="/assets/images/aboutcover.png"
+            width="700"
+            height={500}
+            alt="Artbook"
+            className=" mb-[1rem]"
+          />
+          <Image
+            src="/assets/images/aboutcover.png"
+            width="700"
+            height={500}
+            alt="Artbook"
+            className=" mb-[1rem]"
+          />
+          <Image
+            src="/assets/images/aboutcover.png"
+            width="700"
+            height={500}
+            alt="Artbook"
+            className=" mb-[1rem]"
+          />
+          <Image
+            src="/assets/images/aboutcover.png"
+            width="700"
+            height={500}
+            alt="Artbook"
+            className=" mb-[1rem]"
+          />
+          <Image
+            src="/assets/images/aboutcover.png"
+            width="700"
+            height={500}
+            alt="Artbook"
+            className=" mb-[1rem]"
+          />
+          <Image
+            src="/assets/images/aboutcover.png"
+            width="700"
+            height={500}
+            alt="Artbook"
+            className=" mb-[1rem]"
+          />
+        </div>
+      </Section>
+      <script
+        id="about-json-ld"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(addAboutJsonLd()),
+        }}
+      />
+    </>
+  );
+}
+
+export default About;
